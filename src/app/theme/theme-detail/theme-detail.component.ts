82cbce0192ef17e339cc4dd2fd8dcdc30181b864
Henry Taeschner <henry.taeschner@capgemini.com>
2024-01-31 20:02:17 +0100

    fix: delete theme

--- src/app/theme/theme-detail/theme-detail.component.ts
+++ src/app/theme/theme-detail/theme-detail.component.ts
@@ -18,7 +18,6 @@ export class ThemeDetailComponent implements OnInit {
   theme: Theme | undefined
   usedInWorkspace: Workspace[] | undefined
   themeName!: string
-  themeId!: string
   themeDeleteVisible = false
   themeDeleteMessage = ''
   themePortalList = ''
@@ -174,7 +173,7 @@ export class ThemeDetailComponent implements OnInit {
   }
 
   private deleteTheme(): void {
-    this.themeApi.deleteTheme({ id: this.themeId }).subscribe({
+    this.themeApi.deleteTheme({ id: this.theme?.id! }).subscribe({
       next: () => {
         this.router.navigate(['..'], { relativeTo: this.route })
         this.msgService.success({ summaryKey: 'ACTIONS.DELETE.THEME_OK' })
