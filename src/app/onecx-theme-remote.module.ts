a890ee0c9e34fda9122c40700d2eed230f281874
Christian Badura <christian.badura@capgemini.com>
2024-06-10 15:36:37 +0200

    fix: update libs

--- src/app/onecx-theme-remote.module.ts
+++ src/app/onecx-theme-remote.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
 import {
-  addInitializeModuleGuard,
   AppStateService,
   ConfigurationService,
   createTranslateLoader,
   PortalCoreModule,
   PortalMissingTranslationHandler
 } from '@onecx/portal-integration-angular'
+import { addInitializeModuleGuard } from '@onecx/angular-integration-interface'
 
 const routes: Routes = [
   {
