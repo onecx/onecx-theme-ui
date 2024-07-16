31e35ea13cd467a75af9bb209a6144bffc07203a
Christian Badura <christian.badura@capgemini.com>
2024-07-16 16:17:20 +0200

    fix: finish migration to webcomponent and raise libs

--- src/app/onecx-theme-remote.module.ts
+++ src/app/onecx-theme-remote.module.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser'
 import { APP_INITIALIZER, DoBootstrap, Injector, NgModule } from '@angular/core'
-import { createCustomElement } from '@angular/elements'
 import { Router, RouterModule, Routes } from '@angular/router'
 import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core'
 
@@ -14,7 +13,7 @@ import {
   PortalMissingTranslationHandler
 } from '@onecx/portal-integration-angular'
 import { addInitializeModuleGuard } from '@onecx/angular-integration-interface'
-import { initializeRouter, startsWith } from '@onecx/angular-webcomponents'
+import { createAppEntrypoint, initializeRouter, startsWith } from '@onecx/angular-webcomponents'
 import { AppEntrypointComponent } from './app-entrypoint.component'
 import { AngularAuthModule } from '@onecx/angular-auth'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -69,9 +68,6 @@ export class OneCXThemeModule implements DoBootstrap {
   }
 
   ngDoBootstrap(): void {
-    const appEntrypoint = createCustomElement(AppEntrypointComponent, {
-      injector: this.injector
-    })
-    customElements.define('ocx-theme-component', appEntrypoint)
+    createAppEntrypoint(AppEntrypointComponent, 'ocx-theme-component', this.injector)
   }
 }
