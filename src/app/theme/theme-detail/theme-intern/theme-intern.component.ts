8f1ce4c19e4227c1f6a8c9543955c914b2bc1887
Christian Badura <christian.badura@capgemini.com>
2024-10-02 11:11:01 +0200

    feat: set up slot for list workspaces

--- src/app/theme/theme-detail/theme-intern/theme-intern.component.ts
+++ src/app/theme/theme-detail/theme-intern/theme-intern.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
 
+import { SlotService } from '@onecx/angular-remote-components'
+import { Observable } from 'rxjs'
 import { Theme } from 'src/app/shared/generated'
 
 @Component({
@@ -11,12 +13,21 @@ export class ThemeInternComponent implements OnChanges {
   @Input() theme: Theme | undefined
   @Input() dateFormat = 'medium'
   @Input() workspaceList: string | undefined
+  public isListWorkspacesUsingThemeComponentDefined$: Observable<boolean> | undefined
+  public listWorkspacesUsingThemeSlotName = 'onecx-theme-list-workspaces-using-theme'
 
   @ViewChild('usedInWorkspaces') usedInWorkspaces: ElementRef = {} as ElementRef
 
   public operator = false
 
-  constructor(private translate: TranslateService) {}
+  constructor(
+    private readonly translate: TranslateService,
+    private readonly slotService: SlotService
+  ) {
+    this.isListWorkspacesUsingThemeComponentDefined$ = this.slotService.isSomeComponentDefinedForSlot(
+      this.listWorkspacesUsingThemeSlotName
+    )
+  }
 
   public ngOnChanges(): void {
     if (this.theme) this.operator = this.theme.operator ?? false
