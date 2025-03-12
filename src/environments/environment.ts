82ec102642a96bce14a7bf3988715e762504769f
Henry Taeschner <github@henry-taeschner.de>
2025-03-12 19:21:49 +0100

    fix: setup and code cleanup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   KEYCLOAK_URL: 'http://keycloak-app/',
   KEYCLOAK_REALM: 'OneCX',
-  KEYCLOAK_CLIENT_ID: 'portal-mf-shell',
   TKIT_PORTAL_ID: 'ADMIN',
   skipRemoteConfigLoad: true,
   apiPrefix: 'bff',
