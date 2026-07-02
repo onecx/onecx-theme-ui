266682bde8e0150b6f9995be152ca832d7556bd5
Henry Taeschner <github@henry-taeschner.de>
2026-07-02 08:19:57 +0200

    feat: fix linting

--- src/app/remotes/current-theme-logo/current-theme-logo.component.spec.ts
+++ src/app/remotes/current-theme-logo/current-theme-logo.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { provideHttpClient } from '@angular/common/http'
 import { provideHttpClientTesting } from '@angular/common/http/testing'
 import { provideNoopAnimations } from '@angular/platform-browser/animations'
