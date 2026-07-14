76736b3cc957ba5d97183741ac578437dcfe7e1e
Henry Taeschner <github@henry-taeschner.de>
2026-07-14 10:54:04 +0200

    feat: improve import, extend validation and displaying

--- src/app/theme/theme-import/theme-import.component.ts
+++ src/app/theme/theme-import/theme-import.component.ts
@@ -6,7 +6,8 @@ import {
   inject,
   OnChanges,
   model,
-  input
+  input,
+  Signal
 } from '@angular/core'
 import { HttpHeaders } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms'
@@ -27,6 +28,9 @@ import { PortalMessageService } from '@onecx/angular-integration-interface'
 import { Theme, ThemesAPIService, ThemeSnapshot } from 'src/app/shared/generated'
 import { ThemeColorBoxComponent } from 'src/app/shared/theme-color-box/theme-color-box.component'
 import { ThemeProperties } from 'src/app/shared/models/theme.model'
+import { startWith } from 'rxjs/internal/operators/startWith'
+import { map } from 'rxjs/internal/operators/map'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-theme-import',
@@ -63,52 +67,66 @@ export class ThemeImportComponent implements OnChanges, AfterViewInit {
   // dialog
   public themeNameExists = false
   public displayNameExists = false
-  public themeImportError = false
+  public importError = false
   public themeSnapshot: ThemeSnapshot | null = null
   public httpHeaders!: HttpHeaders
   public properties: ThemeProperties | null = null
-  public formGroup: FormGroup
-
-  constructor() {
-    this.uploaded.set(false)
-    this.formGroup = new FormGroup({
-      themeName: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
-      displayName: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)])
-    })
-  }
+  public formGroup = new FormGroup({
+    themeName: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(100)
+    ]),
+    displayName: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(100)
+    ])
+  })
+  // signals
+  public isFormValid = toSignal(
+    this.formGroup.statusChanges.pipe(
+      map((status) => status === 'VALID'),
+      startWith(this.formGroup.valid) // initial state on component init
+    ),
+    { requireSync: true }
+  )
 
   ngOnChanges(): void {
     if (this.visible()) {
       this.httpHeaders = new HttpHeaders()
       this.httpHeaders = this.httpHeaders.set('Content-Type', 'application/json')
-    } else this.themeSnapshot = null
+    } else this.onImportClear()
   }
 
   ngAfterViewInit() {
     this.cd.detectChanges()
   }
 
-  public async onImportThemeSelect(event: FileSelectEvent): Promise<void> {
-    this.formGroup.reset()
+  public async onImportSelectFile(event: FileSelectEvent): Promise<void> {
+    this.onImportClear()
     return event.files[0].text().then((text) => {
       this.themeSnapshot = null
       try {
         const themeSnapshot = JSON.parse(text)
         if (this.isThemeImportRequestDTO(themeSnapshot)) {
           this.themeSnapshot = themeSnapshot
-          this.themeImportError = false
           if (themeSnapshot.themes) {
             const key: string[] = Object.keys(themeSnapshot.themes)
             this.properties = themeSnapshot.themes[key[0]].properties as ThemeProperties
             this.formGroup.controls['themeName'].setValue(key[0])
-            this.formGroup.controls['displayName'].setValue(themeSnapshot.themes[key[0]].displayName)
+            this.formGroup.controls['displayName'].setValue(themeSnapshot.themes[key[0]].displayName ?? null)
+            if (this.formGroup.controls['displayName'].value === null) {
+              this.formGroup.controls['displayName'].setErrors({ required: true })
+              this.formGroup.controls['displayName'].markAsDirty()
+            }
           }
-          this.cd.markForCheck() // force change detection to update the view with the new properties
           this.onThemeNameChange()
         } else {
           console.error('Theme Import Error: not valid data ')
-          this.themeImportError = true
+          this.importError = true
         }
+        this.cd.markForCheck() // force change detection to update the view with the new properties
       } catch (err) {
         console.error('Theme Import Parse Error', err)
       }
@@ -123,20 +141,25 @@ export class ThemeImportComponent implements OnChanges, AfterViewInit {
     )
   }
 
-  public onImportThemeClear(): void {
+  public onImportClear(): void {
+    this.formGroup.reset()
     this.themeSnapshot = null
-    this.themeImportError = false
+    this.importError = false
+    this.themeNameExists = false
+    this.displayNameExists = false
   }
+
   public onThemeUpload(): void {
+    this.uploaded.set(false)
     if (!this.formGroup.valid || !this.properties) return
     if (!this.themeSnapshot?.themes) return
     // Import data preparation
     const key: string[] = Object.keys(this.themeSnapshot?.themes)
-    this.themeSnapshot.themes[key[0]].displayName = this.formGroup.controls['displayName'].value
+    this.themeSnapshot.themes[key[0]].displayName = this.formGroup.controls['displayName'].value ?? undefined
     if (key[0] !== this.formGroup.controls['themeName'].value) {
       // save the theme properties to be reassigned on new key
       const themeProps = Object.getOwnPropertyDescriptor(this.themeSnapshot.themes, key[0])
-      Object.defineProperty(this.themeSnapshot.themes, this.formGroup.controls['themeName'].value, themeProps!)
+      Object.defineProperty(this.themeSnapshot.themes, this.formGroup.controls['themeName'].value!, themeProps!)
       delete this.themeSnapshot.themes[key[0]]
     }
     // Import execution: upload
@@ -147,7 +170,7 @@ export class ThemeImportComponent implements OnChanges, AfterViewInit {
       .subscribe({
         next: () => {
           this.msgService.success({ summaryKey: 'THEME.IMPORT.IMPORT_THEME_SUCCESS' })
-          this.onImportThemeClear()
+          this.onImportClear()
           this.uploaded.set(true)
           this.router.navigate([`./${this.formGroup.controls['themeName'].value}`], { relativeTo: this.route })
         },
