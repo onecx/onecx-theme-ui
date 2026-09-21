a2b53a88cf196633439b9e830e6e726f7a1a5b92
Henry Taeschner <github@henry-taeschner.de>
2026-09-21 20:27:14 +0200

    fix: fix theme search init tests for actions

--- src/app/theme/theme-search/theme-search.component.spec.ts
+++ src/app/theme/theme-search/theme-search.component.spec.ts
@@ -84,16 +84,30 @@ describe('ThemeSearchComponent', () => {
       })
     })
 
-    it('should init actions on initialization', () => {
+    it('should init actions on initialization - size', () => {
       let actions: any = []
+
       component.actions$!.subscribe((act) => (actions = act))
+
       expect(actions).toHaveSize(2)
+    })
 
+    it('should init actions on initialization - create', () => {
+      let actions: any = []
+
+      component.actions$!.subscribe((act) => (actions = act))
       actions[0].actionCallback()
-      expect(component.themeCreateVisible()).toBeTrue()
 
+      expect(component.themeCreateVisible()).toBeTrue()
+    })
+
+    it('should init actions on initialization - import', () => {
+      let actions: any = []
       spyOn(component, 'onImportThemeClick')
+
+      component.actions$!.subscribe((act) => (actions = act))
       actions[1].actionCallback()
+
       expect(component.onImportThemeClick).toHaveBeenCalledTimes(1)
     })
   })
@@ -155,7 +169,6 @@ describe('ThemeSearchComponent', () => {
   })
 
   it('should change field to sort by on sort change', () => {
-    fixture.detectChanges()
     component.sortField = 'name'
 
     component.onSortChange({ sortColumn: 'description', sortDirection: DataSortDirection.DESCENDING })
@@ -164,8 +177,6 @@ describe('ThemeSearchComponent', () => {
   })
 
   it('should show import dialog on import theme click', () => {
-    fixture.detectChanges()
-
     component.themeImportVisible.set(false)
     component.onImportThemeClick()
 
@@ -173,24 +184,20 @@ describe('ThemeSearchComponent', () => {
   })
 
   it('should hide import dialog on import close and reload', () => {
-    fixture.detectChanges()
     spyOn(component, 'loadThemes')
 
     component.themeImported.set(true)
     fixture.detectChanges()
-    TestBed.flushEffects()
 
     expect(component.loadThemes).toHaveBeenCalledTimes(1)
   })
 
   it('should navigate to created theme on theme creation', () => {
-    fixture.detectChanges()
     const router = TestBed.inject(Router)
     spyOn(router, 'navigate').and.returnValue(Promise.resolve(true))
 
     component.onThemeCreation({ name: 'new-theme' } as Theme)
     fixture.detectChanges()
-    TestBed.flushEffects()
 
     expect(router.navigate).toHaveBeenCalledWith(['./new-theme'], { relativeTo: (component as any).route })
   })
